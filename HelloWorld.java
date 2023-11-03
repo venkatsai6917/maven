7551e8ad32ea04fb76d6f517fd875cb3862c2b48
venkat sai <venkatsai6917@gmail.com>
2023-11-03 22:54:10 +0530

    mavencommit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+		System.out.println("Hi this is venkat"); 
+		System.out.println("Hey whats up buddy"); 
+	}
+}
